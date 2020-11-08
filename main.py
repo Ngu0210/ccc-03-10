bbc294e3d69fb01b85c5b8047e00bff608dd11e8
Danny Nguyen <slickboi@slim.local>
2020-11-08 18:53:47 +1100

    Added changes

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask, request, jsonify
 app = Flask(__name__)
-from controller import registerable_controllers
+
+from database import init_db
+db = init_db(app)
+
+from controllers import registerable_controllers
 
 for controller in registerable_controllers:
     app.register_blueprint(controller)
